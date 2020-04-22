f719f4839410247a50b91fd92b1c762dbe935168
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-22 16:27:50 -0400

    Updating src/compontents/Cards.jsx - Basic Strucutre.
    This commit updates the cards components to have the basic
    structure of what the JSX is going to look like. I still need
    to retrieve the data from the props and display that in the JSX.

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,75 @@
-import React from 'react';
+/** 
+ * This module is the cards component.
+*/
 
-const Cards = () => {
+// Imports.
+import React from 'react';
+import styles from './Cards.module.css';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+
+/** 
+ * Cards Functional Component.
+*/
+const Cards = (props) => {
+
+    /** 
+     * JSX for the card component
+    */
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                           Infected
+                        </Typography>  
+                        <Typography variant="h5">
+                            REAL DATA
+                        </Typography> 
+                        <Typography color="textSecondary">
+                            REAL DATE
+                        </Typography> 
+                        <Typography variant="body2">
+                            Number of active cases of COVID-19
+                        </Typography> 
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Recovered
+                        </Typography>  
+                        <Typography variant="h5">
+                            REAL DATA
+                        </Typography> 
+                        <Typography color="textSecondary">
+                            REAL DATE
+                        </Typography> 
+                        <Typography variant="body2">
+                            Number of recoveries from COVID-19
+                        </Typography> 
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>
+                            Deaths
+                        </Typography>  
+                        <Typography variant="h5">
+                            REAL DATA
+                        </Typography> 
+                        <Typography color="textSecondary">
+                            REAL DATE
+                        </Typography> 
+                        <Typography variant="body2">
+                            Number of deaths caused by COVID-19
+                        </Typography> 
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
+// Export the Cards functional component.
 export default Cards;
