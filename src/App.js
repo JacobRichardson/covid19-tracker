4cdbb7de7a29f0c6b4dd70293f65a770a6d0ed33
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-22 16:25:31 -0400

    Updating src/App.js - Fetching API data.
    This commit updates the App compontent to fetch the data from
    the API when the component mounts as well as passing that data
    to the cards component as props. This commit also includes some
    comments as well.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React from 'react';
+/**
+ * This module is the main app component.
+ */
 
+// Imports.
+import React from 'react';
 import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css'
+import { fetchData } from './api';
+
+/**
+ * The main class for the app component.
+ * @class App
+ * @extends {React.Component}
+ */
 class App extends React.Component {
 
+    // The state of the component.
+    state = {
+        data: {}
+    }
+
+    /**
+     * Function that is executed when the component mounts.
+     * Retrieve the data from the API.
+     * @memberof App
+     */
+    async componentDidMount() {
+
+        // Retrieve the data using the fetch data function.
+        const fetchedData = await fetchData();
+
+        // Set the data onto the state.
+        this.setState({
+            data: fetchedData
+        });
+    }
+
+    /**
+     * The render function renders the app.
+     * @returns The JSX for the app.
+     * @memberof App
+     */
     render() {
+
+        // Retrieve the data from the state.
+        const { data } = this.state;
+
         return (
             <div className={styles.container}>
-                <Cards />
+                <Cards data={data}/>
                 <CountryPicker />
                 <Chart />
             </div>
@@ -15,4 +56,5 @@ class App extends React.Component {
     }
 }
 
+// Export the App class.
 export default App;
