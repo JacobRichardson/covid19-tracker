babc3657a1214f229a0ff9c4e101628910811149
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-24 13:33:48 -0400

    Updating src/App.js - Passing data and country.
    This commit updates the App component to pass the data and the
    country as props to the chart component. This information is what
    is going to be used to display in the cart.

--- src/App.js
+++ src/App.js
@@ -62,13 +62,13 @@ class App extends React.Component {
     render() {
 
         // Retrieve the data from the state.
-        const { data } = this.state;
+        const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
                 <Cards data={data}/>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
-                <Chart />
+                <Chart data={data} country={country} />
             </div>
         )
     }
