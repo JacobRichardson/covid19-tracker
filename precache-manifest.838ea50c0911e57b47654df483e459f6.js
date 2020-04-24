65030c4de4e0f355f99ecdde2775a985be04cd3a
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-24 14:05:51 -0400

    Updates

---
+++ precache-manifest.838ea50c0911e57b47654df483e459f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19-tracker/static/media/COVID-19.d7265326.png"
+  },
+  {
+    "revision": "15d1e3192aabd61d4134",
+    "url": "/covid19-tracker/static/js/runtime~main.90d67456.js"
+  },
+  {
+    "revision": "798c81b7ad132be80a06",
+    "url": "/covid19-tracker/static/js/main.d616ed7c.chunk.js"
+  },
+  {
+    "revision": "8ea8c05dd279ea69d0d6",
+    "url": "/covid19-tracker/static/js/2.2fa8777c.chunk.js"
+  },
+  {
+    "revision": "798c81b7ad132be80a06",
+    "url": "/covid19-tracker/static/css/main.1693de14.chunk.css"
+  },
+  {
+    "revision": "ff19ae4e2bf3031fdb14a81677e2aba7",
+    "url": "/covid19-tracker/index.html"
+  }
+];
