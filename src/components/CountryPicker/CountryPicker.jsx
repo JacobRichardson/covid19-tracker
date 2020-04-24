f7e887b764854a2ac553c1f8301448760ef00e52
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-04-24 12:36:23 -0400

    Updating src/components/CountryPicker/CountryPicker.js
    This commit updates the country picker to fetch the country
    data and display all countries as options in the dropdown.
    This commit also makes it so that the selected country is passed
    back to the parent app compontent.

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,57 @@
-import React from 'react';
+/**
+ * Country Picker Component.
+*/
 
-const CountryPicker = () => {
+// Imports.
+import React, { useState, useEffect }from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+import { fetchCountries } from '../../api';
+
+/**
+ * Functional Component.
+*/
+const CountryPicker = (props) => {
+
+    // Create the state using use state.
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    // Use effect function.
+    useEffect(() => {
+
+        // Fetch API async function.
+        const fetchAPI = async () => {
+
+            // Set the countries from the api onto the state.
+            setFetchedCountries(await fetchCountries());
+        }
+
+        // Invoke the function.
+        fetchAPI();
+
+    }, [setFetchedCountries]);
+
+    /**
+     * Return JSX.
+    */
     return (
-        <h1>Country Picker</h1>
+       <FormControl className={styles.formControl}>
+
+           {/* Invoke the handle country change from props when on change is called. */}
+           <NativeSelect defaultValue="" onChange={(e) => props.handleCountryChange(e.target.value)}>
+
+               {/* Map over the countries */}
+                {fetchedCountries.map((country, i) =>
+                
+                    // Create an option using the country.
+                    <option key ={i} value={country}>
+                        {country}
+                    </option>
+                )}
+           </NativeSelect>
+       </FormControl>
     )
 }
 
+// Export the country picker component.
 export default CountryPicker;
